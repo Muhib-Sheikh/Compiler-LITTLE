8137cea33baa50aeeeb7998422835016fadc94d7
Muhib Sheikh <muhib7005@gmail.com>
2022-01-30 12:33:32 -0500

    basic files

--- Driver.java
+++ Driver.java
@@ -2,10 +2,8 @@ import org.antlr.v4.runtime.*;
 //import org.antlr.v4.runtime.tree.*;
 import java.util.Hashtable;
 
-public class Driver
-{
-    public static void main(String[] args) throws Exception
-    {
+public class Driver {
+    public static void main(String[] args) throws Exception {
         Hashtable<Integer, String> tokens = new Hashtable<Integer, String>();
         tokens.put(1, "KEYWORD");
         tokens.put(2, "IDENTIFIER");
@@ -15,19 +13,17 @@ public class Driver
         tokens.put(6, "COMMENT");
         tokens.put(7, "OPERATOR");
 
-        //creates a CharStream that reads from standard input
-        //ANTLRInputStream input = new ANTLRInputStream(System.in);
+        // creates a CharStream that reads from standard input
+        // ANTLRInputStream input = new ANTLRInputStream(System.in);
         CharStream input = CharStreams.fromString(tokens.get(1) + tokens.get(2) + tokens.get(3) +
                 tokens.get(4) + tokens.get(5) + tokens.get(6) + tokens.get(7));
 
-        //lexer that feeds off of input CharStream
+        // lexer that feeds off of input CharStream
         Little lexer = new Little(input);
 
-        while(true)
-        {
+        while (true) {
             Token token = lexer.getToken();
-            if(token.getType() == Token.EOF)
-            {
+            if (token.getType() == Token.EOF) {
                 break;
             }
 
--- Little.g4
+++ Little.g4
@@ -1,10 +1,42 @@
 grammar Little;
 
 r: COMMENT;
-KEYWORD : 'PROGRAM' | 'BEGIN' | 'END' | 'FUNCTION' | 'READ' | 'WRITE' | 'IF' | 'ELSE' | 'ENDIF' | 'WHILE' | 'ENDWHILE' | 'CONTINUE' | 'BREAK' | 'RETURN' | 'INT' | 'VOID' | 'STRING' | 'FLOAT' ;
-IDENTIFIER : ([a-z]|[A-Z]) [A-Za-z0-9]* ;
-INTLITERAL : [0-9]+ ;
-FLOATLITERAL : [0-9]* '.' [0-9]+ ;
-STRINGLITERAL : '"' .*? '"' ;
-COMMENT : '--' .*? [EOF\n\r]-> skip;
-OPERATOR : ':=' | '+' | '-' | '/' | '=' | '!=' | '<' | '>' | '(' | ')' | ';' | ',' | '<=' | '>=' ;
+KEYWORD:
+	'PROGRAM'
+	| 'BEGIN'
+	| 'END'
+	| 'FUNCTION'
+	| 'READ'
+	| 'WRITE'
+	| 'IF'
+	| 'ELSE'
+	| 'ENDIF'
+	| 'WHILE'
+	| 'ENDWHILE'
+	| 'CONTINUE'
+	| 'BREAK'
+	| 'RETURN'
+	| 'INT'
+	| 'VOID'
+	| 'STRING'
+	| 'FLOAT';
+IDENTIFIER: ([a-z] | [A-Z]) [A-Za-z0-9]*;
+INTLITERAL: [0-9]+;
+FLOATLITERAL: [0-9]* '.' [0-9]+;
+STRINGLITERAL: '"' .*? '"';
+COMMENT: '--' .*? [EOF\n\r] -> skip;
+OPERATOR:
+	':='
+	| '+'
+	| '-'
+	| '/'
+	| '='
+	| '!='
+	| '<'
+	| '>'
+	| '('
+	| ')'
+	| ';'
+	| ','
+	| '<='
+	| '>=';
